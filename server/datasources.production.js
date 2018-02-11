722180d003bd5d40b591f8727427511d425d3cd0
Kaushik Subramaniam <kaushik.subman@gmail.com>
2018-02-10 23:47:40 -0600

    Reverting to non docker based deployment

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,6 +2,7 @@ var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||
   'mongodb://';
 module.exports = {
   mongodb: {
-    url: mongoUri
+    url: mongoUri,
+    uri_decode_auth: true
   }
 };
